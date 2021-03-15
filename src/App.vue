58ba20ff3ddae810dea91fdc2ed84680f75dae86
Audrey Thomasson <audrey@macbook-pro.attlocal.net>
2021-03-15 12:26:52 -0500

    movie details showing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  
+  <v-app>
+    <app-header />
+    <v-main>
+      <v-container>
+        <search-results />
+        <v-row class="mt-6">
+          <v-col :sm="12" :md="6">
+            <movie-details />
+          </v-col>
+          <v-col :sm="12" :md="6">
+            <movie-reviews />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {
+import AppHeader from "./components/AppHeader";
+import MovieDetails from "./components/MovieDetails";
+import SearchResults from "./components/SearchResults";
+import MovieReviews from "./components/MovieReviews";
 
-}
+export default {
+  components: {
+    SearchResults,
+    AppHeader,
+    MovieDetails,
+    MovieReviews
+  }
+};
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,60 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    searchResults: { total_results: 0, results: [] },
+    movieDetails: null,
+    reviews: [],
+    searchTerm: ""
+  },
+
+  mutations: {
+    setSearchTerm(state, newSearchTerm) {
+      state.searchTerm = newSearchTerm;
+    },
+    setSearchResults(state, payload) {
+      state.searchResults = payload;
+    },
+    setMovieDetails(state, payload) {
+      state.movieDetails = payload;
+    },
+    setMovieReviews(state, payload) {
+      state.reviews = payload;
+    }
+  },
+  actions: {
+    async searchMovies({ commit }, query) {
+      const apiBaseUrl = "https://api.themoviedb.org/3";
+      const apiKey = process.env.VUE_APP_MOVIE_API_KEY;
+      const url = `${apiBaseUrl}/search/movie?query=${query}&api_key=${apiKey}`;
+
+      const res = await fetch(url);
+      const data = await res.json();
+      commit("setSearchTerm", query);
+      commit("setSearchResults", data);
+      commit("setMovieDetails", null);
+    },
+    async selectMovie({ commit }, movie) {
+      const apiBaseUrl = "https://api.themoviedb.org/3";
+      const apiKey = process.env.VUE_APP_MOVIE_API_KEY;
+      const movieDetailsUrl = `${apiBaseUrl}/movie/${movie.id}?api_key=${apiKey}`;
+      const movieReviewsUrl = `${apiBaseUrl}/movie/${movie.id}/reviews?api_key=${apiKey}`;
+    
+    //get movie details
+    const detailsRes = await fetch(movieDetailsUrl);
+    const details = await detailsRes.json();
+    commit("setMovieDetails", details);
+
+    //get movie reviews
+    const reviewsRes = await fetch(movieReviewsUrl);
+    const reviewData = await reviewsRes.json();
+    commit("setMovieReviews", reviewData.results);
+    },
+  },
+  modules: {},
+  getters: {
+    viewableMovies(state) {
+      return state.searchResults.results.filter(r => r.poster_path);
+    }
+  }
 });
